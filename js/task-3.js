c4c035f34b60d3aa695b09227f10314610c13504
KOVALCHUK ANNA <annakv545@gmail.com>
2026-07-06 19:34:12 +0300

    Add files via upload

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+const profile = {
+    username: "Jacob",
+  playTime: 300,
+
+changeUsername(newName){
+    this.username===newName;
+},
+updatePlayTime(hours){
+    this.playTime += hour;
+},
+getInfo(){
+    return `${this.username} has ${this.playTim} active hours!`;
+},
+};
