6fa663576c7b00470f0ffd163aafd9fe5acc7fec
KOVALCHUK ANNA <annakv545@gmail.com>
2026-07-06 19:55:31 +0300

    Add files via upload

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,21 @@
+
 const profile = {
-    username: "Jacob",
+  username: "Jacob",
   playTime: 300,
 
-changeUsername(newName){
-    this.username===newName;
-},
-updatePlayTime(hours){
+  changeUsername(newName) {
+    this.username = newName;
+  },
+
+  updatePlayTime(hours) {
     this.playTime += hours;
-},
-getInfo(){
-    return `${this.username} has ${this.playTim} active hours!`;
-},
+  },
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
 };
+
 
 
 
